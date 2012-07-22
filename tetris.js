f7afd098b30648c654db309336cdeabab6bf0a8a
me-andre <andrew.pazniak@gmail.com>
2012-07-22 15:22:21 +0300

    using closures to isolate variables

--- tetris.js
+++ tetris.js
@@ -1,14 +1,16 @@
-var countPress = 1
+var setBg
 
-var setBg = function ()
-{
-  var newColor
-  switch (countPress++)
-  {
-    case 2: newColor = 'red'; break
-    case 3: newColor = 'green'; break
-    case 4: newColor = 'blue'; break
-    case 5: newColor = 'white'; countPress = 1
+(function () { // создаём анонимную функцию, чтобы спрятать переменные
+  var countPress = 1
+
+  setBg = function () {
+    var newColor
+    switch (countPress++) {
+      case 2: newColor = 'red'; break
+      case 3: newColor = 'green'; break
+      case 4: newColor = 'blue'; break
+      case 5: newColor = 'white'; countPress = 1
+    }
+    document.bgColor = newColor
   }
-  document.bgColor = newColor
-}
+})()
